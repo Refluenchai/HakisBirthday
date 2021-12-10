35df13c3436bb4b5363e3fbb820363d38ecc8cc7
Victor Queiroz <victorqs1997@hotmail.com>
2021-12-10 16:18:59 -0300

    second commit

--- src/main/kotlin/wallpaper/WallpaperChanger.java
+++ src/main/kotlin/wallpaper/WallpaperChanger.java
@@ -6,52 +6,48 @@ import com.sun.jna.win32.W32APIOptions;
 
 import javax.swing.*;
 import java.io.*;
+import java.util.Objects;
 
 public class WallpaperChanger {
-    public static interface User32 extends Library {
-        User32 INSTANCE = (User32) Native.loadLibrary("user32",User32.class,W32APIOptions.DEFAULT_OPTIONS);
-        boolean SystemParametersInfo(int one, int two, String s, int three);
+    public interface User32 extends Library {
+        User32 INSTANCE = Native.load("user32",User32.class,W32APIOptions.DEFAULT_OPTIONS);
+        void SystemParametersInfo(int one, int two, String s, int three);
     }
     public static void ChangeDesktopBackgroundOnWindows(String ruta){
         User32.INSTANCE.SystemParametersInfo(0x0014, 0, ruta , 1);
     }
 
     public static void ChangeDesktopBackgroundOnOSX(final String filename) {
-        SwingUtilities.invokeLater(new Runnable()
-        {
-            public void run()
+        SwingUtilities.invokeLater(() -> {
+            File file;
+            try
             {
-                File file = null;
-                try
-                {
-                    file = new File(filename);
+                file = new File(filename);
 
-                    String as[] = {
-                            "osascript",
-                            "-e", "tell application \"Finder\"",
-                            "-e", "set desktop picture to POSIX file \"" + file.getAbsolutePath() + "\"",
-                            "-e", "end tell"
-                    };
-                    Runtime runtime = Runtime.getRuntime();
-                    Process process;
-                    process = runtime.exec(as);
-                }
-                catch (Exception e)
-                {
-                    e.printStackTrace();
-                    System.exit(1);
-                }
+                String[] as = {
+                        "osascript",
+                        "-e", "tell application \"Finder\"",
+                        "-e", "set desktop picture to POSIX file \"" + file.getAbsolutePath() + "\"",
+                        "-e", "end tell"
+                };
+                Runtime runtime = Runtime.getRuntime();
+                runtime.exec(as);
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+                System.exit(1);
             }
         });
     }
     public void rodar() {
 
-        String ruta="";
+        String ruta;
 
-        ruta=InputStreamAFile(WallpaperChanger.class.getResourceAsStream("devilmaycrywallpaper.jpg"),"devilmaycrywallpaper.jpg");
-        System.out.printf("path:"+ruta);
+        ruta=InputStreamAFile(Objects.requireNonNull(WallpaperChanger.class.getResourceAsStream("devilmaycrywallpaper.jpg")),"devilmaycrywallpaper.jpg");
+        System.out.print("path:"+ruta);
 
-        System.out.printf("\nSO:"+System.getProperty("os.name"));
+        System.out.print("\nSO:"+System.getProperty("os.name"));
 
         if(System.getProperty("os.name").toLowerCase().contains("windows")){
             ChangeDesktopBackgroundOnWindows(ruta);
@@ -67,9 +63,6 @@ public class WallpaperChanger {
 
         File borrar = new File(ruta );
         borrar.delete();
-
-
-
     }
     public static String InputStreamAFile(InputStream entrada,String archivoDestino){
         String retorno= "";
@@ -87,7 +80,7 @@ public class WallpaperChanger {
 
             //System.out.println("Se realizo la conversion con exito");
         }catch(IOException e){
-            System.out.println("Se produjo el error : "+e.toString());
+            System.out.println("Se produjo el error : "+ e);
         }
         return retorno;
     }
